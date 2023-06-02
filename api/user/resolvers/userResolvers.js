95f011f695cf69466b523fb14731c13bd47c6dbc
Luis Martins <chateadorn@gmail.com>
2023-06-02 09:30:36 -0300

    removing default array of users and implementing dataSource as resolver

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,21 +1,8 @@
-
 const userResolvers = {
     Query: {
-        users: () => users,
-        firstUser: () => users[0]
+        users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers()
     }
 
 };
-
-const users = [
-    {
-        nome: "Luis",
-        ativo: true
-    },
-    {
-        nome: "Rutileno",
-        ativo: false
-    }
-]
 
 module.exports = userResolvers;
