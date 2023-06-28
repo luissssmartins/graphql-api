3aec36e956ecf51242a34686ee986cdf958b508e
Luis Martins <chateadorn@gmail.com>
2023-06-27 22:52:41 -0300

    adicionando mutation

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,8 +2,11 @@ const userResolvers = {
     Query: {
         users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
         user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
-    }
+    },
 
+    Mutation: {
+        createUser: async(root, user, { dataSources }) => dataSources.usersAPI.createUser(user)
+    }
 };
 
 module.exports = userResolvers;
