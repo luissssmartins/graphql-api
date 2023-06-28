de338e299bd0480369ffc25c7bb61a0a7854664f
Luis Martins <chateadorn@gmail.com>
2023-06-27 22:52:18 -0300

    create user

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -19,13 +19,28 @@ class UsersAPI extends RESTDataSource {
             role: await this.get(`/roles/${user.role}`)
         }));
     }
-//
+
     async getUserById(id) {
         const user = this.get(`/users/${id}`);
 
         user.role = this.get(`/roles/${user.role}`);
 
         return user;
+    }
+
+    async createUser(user) {
+        const users = await this.get('/users');
+
+        user.id = users.length + 1;
+
+        const role = await this.get(`roles?type=${user.role}`);
+
+        await this.post('users', {...user, role: role[0].id});
+
+        return ({
+            ...user,
+            role: role[0]
+        });
     }
 }
 
